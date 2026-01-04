98e673c1d570d3cd5144d09b723156e19654dbec
vomw <22770640+vomw@users.noreply.github.com>
2026-01-04 18:57:05 +0800

    detect browser language

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(HttpApi)
+  .use(LanguageDetector) // Add LanguageDetector
   .use(initReactI18next)
   .init({
     supportedLngs: ['en', 'zh'],
@@ -11,6 +13,10 @@ i18n
     debug: true,
     backend: {
       loadPath: `${import.meta.env.BASE_URL}resume_{{lng}}.json`,
+    },
+    detection: {
+      order: ['navigator', 'querystring', 'cookie', 'localStorage', 'sessionStorage', 'htmlTag', 'path', 'subdomain'],
+      caches: ['localStorage'],
     },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
