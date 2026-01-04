0ea9cab7db062056893538c4101cc832d7d8cde5
vomw <22770640+vomw@users.noreply.github.com>
2026-01-04 18:29:47 +0800

    use local npm package instead of cdn

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './i18n';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
