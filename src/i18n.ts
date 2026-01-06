52f20fa3daa44da60fa27bef410ffd3f9a78b2db
vomw <22770640+vomw@users.noreply.github.com>
2026-01-06 17:03:17 +0800

    b64

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,19 @@ import { initReactI18next } from 'react-i18next';
 import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+// Function to decode Base64
+const atob_utf8 = (b64: string) => {
+  return decodeURIComponent(
+    atob(b64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+};
+
+
 i18n
   .use(HttpApi)
   .use(LanguageDetector) // Add LanguageDetector
@@ -12,7 +25,8 @@ i18n
     fallbackLng: 'en',
     debug: true,
     backend: {
-      loadPath: `${import.meta.env.BASE_URL}resume_{{lng}}.json`,
+      loadPath: `${import.meta.env.BASE_URL}resume_{{lng}}.b64`,
+      parse: (data: string) => JSON.parse(atob_utf8(data)),
     },
     detection: {
       order: ['navigator', 'querystring', 'cookie', 'localStorage', 'sessionStorage', 'htmlTag', 'path', 'subdomain'],
